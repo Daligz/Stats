7cea88264d058fbb7871495912a3ec40894381ae
SrPandaStick <oscardali370@gmail.com>
2021-03-11 21:27:39 -0600

    Datasource implemented

--- src/main/java/net/royalmind/stats/Stats.java
+++ src/main/java/net/royalmind/stats/Stats.java
@@ -1,12 +1,14 @@
 package net.royalmind.stats;
 
 import net.royalmind.stats.configuration.Files;
+import net.royalmind.stats.data.DataSource;
 import net.royalmind.stats.data.containers.stats.StatsContainerImpl;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Stats extends JavaPlugin {
 
     private Files files;
+    private DataSource dataSource;
 
     //Containers
     private StatsContainerImpl statsContainer;
@@ -15,6 +17,7 @@ public final class Stats extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         this.files = new Files(this);
+        this.dataSource = new DataSource(this);
         this.statsContainer = new StatsContainerImpl();
     }
 
