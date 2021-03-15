0c63539826742da50b84e6758b62a23d328a016a
SrPandaStick <oscardali370@gmail.com>
2021-03-15 15:33:13 -0600

    Chat hover message added

--- src/main/java/net/royalmind/stats/handlers/PlayerDataHandler.java
+++ src/main/java/net/royalmind/stats/handlers/PlayerDataHandler.java
@@ -1,13 +1,20 @@
 package net.royalmind.stats.handlers;
 
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.royalmind.stats.data.DataSource;
 import net.royalmind.stats.data.containers.stats.StatsContainerImpl;
+import net.royalmind.stats.data.containers.stats.StatsDataContainer;
 import net.royalmind.stats.data.containers.threads.ThreadsContainerImpl;
+import net.royalmind.stats.utils.Chat;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -84,5 +91,42 @@ public class PlayerDataHandler implements Listener {
     @EventHandler
     public void onWorldLoad(final WorldLoadEvent event) {
         this.statsContainer.loadWorld(event.getWorld(), this.dataSource, this.plugin);
+    }
+
+    @EventHandler (priority = EventPriority.HIGHEST)
+    public void onChat(final AsyncPlayerChatEvent event) {
+        final Player player = event.getPlayer();
+        String message = event.getMessage();
+        event.setCancelled(true);
+        if (message.contains("/")) return;
+        final StatsDataContainer dataContainer = this.statsContainer.get(player.getUniqueId());
+        final StringBuilder textBuilder = new StringBuilder();
+        for (String text : this.config.getStringList("Chat.Hover")) {
+            text = text.replace("%rs_name%", player.getName());
+            text = text.replace("%rs_kills%", String.valueOf(dataContainer.getKills()));
+            text = text.replace("%rs_deaths%", String.valueOf(dataContainer.getDeaths()));
+            text = text.replace("%rs_bestkillstreak%", String.valueOf(dataContainer.getBestKillStreak()));
+            text = text.replace("%rs_currentkillstreak%", String.valueOf(dataContainer.getCurrentKillStreak()));
+            if (text.contains("_n")) {
+                text = text.replace("_n", "");
+                textBuilder.append(Chat.translate(text) + "\n");
+            } else {
+                textBuilder.append(Chat.translate(text));
+            }
+        }
+        final String coloredPermission = this.config.getString("Chat.ColoredMessage-Permission");
+        if (player.hasPermission(coloredPermission)) {
+            message = Chat.translate(message);
+        }
+        final String chatFormat = this.config.getString("Chat.Message")
+                .replace("%player%", player.getName())
+                .replace("%message%", message);
+
+        TextComponent textComponent = new TextComponent(chatFormat);
+        textComponent.setHoverEvent(new HoverEvent(
+                HoverEvent.Action.SHOW_TEXT,
+                new ComponentBuilder(textBuilder.toString()).create()
+        ));
+        this.plugin.getServer().spigot().broadcast(textComponent);
     }
 }
